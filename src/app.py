500787a1956f1972252a5ce7b000e5c9c059e3e3
NicolaVilla <nicovillarpando@outlook.es>
2023-05-14 21:57:04 -0300

    fix: añadi comentarios

--- src/app.py
+++ src/app.py
@@ -1,8 +1,8 @@
 import tkinter
-
+#Ventana principal tkinter
 ventana = tkinter.Tk()
 ventana.geometry("300x300")
-
+#Texto donde se va a mostrar el resultado
 text_resultado = tkinter.Text(ventana,height="2",width="16")
 text_resultado.grid(columnspan=5)
 
