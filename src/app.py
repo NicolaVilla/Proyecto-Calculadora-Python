001e8775dd523e4b5277e2638f533d8fcfc9280c
NicolaVilla <nicovillarpando@outlook.es>
2023-05-14 21:54:03 -0300

    feat: agregar texto de resultado

--- src/app.py
+++ src/app.py
@@ -2,4 +2,14 @@ import tkinter
 
 ventana = tkinter.Tk()
 ventana.geometry("300x300")
+
+text_resultado = tkinter.Text(ventana,height="2",width="16")
+text_resultado.grid(columnspan=5)
+
+
+
+
+
+
 ventana.mainloop()
+
